b55da3c366559f952ea9eeaac6d8ab2795a1d86e
Soroush <s_shemshadi@ymail.com>
2022-01-20 11:53:08 +0330

    [IMPLEMENT] dice type

---
+++ src/main/java/io/github/shuoros/javagram/type/Dice.java
@@ -0,0 +1,17 @@
+package io.github.shuoros.javagram.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Dice {
+
+    private String emoji;
+    private int value;
+
+}
