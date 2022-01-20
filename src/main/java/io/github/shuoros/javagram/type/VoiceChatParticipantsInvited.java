9aaf5b50c88a03eeaf2cef821c161e3dde6fd60a
Soroush <s_shemshadi@ymail.com>
2022-01-20 12:18:09 +0330

    [IMPLEMENT] voice chat participants invited type

---
+++ src/main/java/io/github/shuoros/javagram/type/VoiceChatParticipantsInvited.java
@@ -0,0 +1,18 @@
+package io.github.shuoros.javagram.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class VoiceChatParticipantsInvited {
+
+    private List<User> users;
+
+}
