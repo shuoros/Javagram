3856b9368cf847b253ed3fcbf75946b758ad19d7
Soroush <s_shemshadi@ymail.com>
2022-03-27 11:36:45 +0430

    UPDATE(getMe method) Update REQUEST path. Remove unneccesary Lombok annotations.

--- src/main/java/io/github/shuoros/javagram/method/GetMe.java
+++ src/main/java/io/github/shuoros/javagram/method/GetMe.java
@@ -2,13 +2,7 @@ package io.github.shuoros.javagram.method;
 
 import io.github.shuoros.javagram.type.Type;
 import io.github.shuoros.javagram.type.User;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
 public class GetMe implements Method {
 
     private final String REQUEST = "/sendMessage";
