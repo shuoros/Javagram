6ae5c1ce9cae64ecbb31693e9146071255e7e022
Soroush <s_shemshadi@ymail.com>
2022-04-12 13:08:13 +0430

    IMPLEMENT(BanChatSenderChat method)

---
+++ src/main/java/io/github/shuoros/javagram/method/BanChatSenderChat.java
@@ -0,0 +1,32 @@
+package io.github.shuoros.javagram.method;
+
+import io.github.shuoros.javagram.type.Boolean;
+import io.github.shuoros.javagram.type.Type;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class BanChatSenderChat implements Method {
+
+    private final String REQUEST = "/setChatAdministratorCustomTitle";
+    private String chatId;
+    private int senderChatId;
+
+    @Override
+    public String getMethod() {
+        return this.REQUEST;
+    }
+
+    @Override
+    public Type getReturnType() {
+        return new Boolean();
+    }
+
+    public BanChatSenderChat(int chatId, int senderChatId) {
+        this(String.valueOf(chatId), senderChatId);
+    }
+
+}
