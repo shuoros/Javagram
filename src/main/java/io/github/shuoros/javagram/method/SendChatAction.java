89f24bc352056ed1b3d45831adcfa49975133311
Soroush <s_shemshadi@ymail.com>
2022-04-12 12:36:14 +0430

    IMPLEMENT(SendChatAction method)

---
+++ src/main/java/io/github/shuoros/javagram/method/SendChatAction.java
@@ -0,0 +1,28 @@
+package io.github.shuoros.javagram.method;
+
+import io.github.shuoros.javagram.type.Boolean;
+import io.github.shuoros.javagram.type.Type;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class SendChatAction implements Method {
+
+    private final String REQUEST = "/sendChatAction";
+    private int chatId;
+    private String action;
+
+    @Override
+    public String getMethod() {
+        return this.REQUEST;
+    }
+
+    @Override
+    public Type getReturnType() {
+        return new Boolean();
+    }
+
+}
